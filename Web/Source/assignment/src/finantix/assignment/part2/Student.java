401e1cb51e70117d9c78b53d12fd421f9b6328d5
Mai Anh Phuong <phuong.anh@open.ac.uk>
2016-06-03 18:47:33 +0700

    Fix Student class using Externalizable

--- Web/Source/assignment/src/finantix/assignment/part2/Student.java
+++ Web/Source/assignment/src/finantix/assignment/part2/Student.java
@@ -1,13 +1,19 @@
 package finantix.assignment.part2;
 
-import java.io.Serializable;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 
-public class Student implements Serializable {
+public class Student implements Externalizable {
 	private static final long serialVersionUID = 4174367359358490807L;
 
 	private String name;
 	private int age;
 	private String className;
+
+	public Student() {
+	}
 
 	public Student(String name, int age, String className) {
 		this.name = name;
@@ -30,5 +36,19 @@ public class Student implements Serializable {
 	public String toString() {
 		return String.format("Student{'name': '%s', 'age': %d, 'className': '%s',}", this.name, this.age,
 				this.className);
+	}
+
+	@Override
+	public void writeExternal(ObjectOutput out) throws IOException {
+		out.writeUTF(this.name);
+		out.writeInt(this.age);
+		out.writeUTF(this.className);
+	}
+
+	@Override
+	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+		this.name = in.readUTF();
+		this.age = in.readInt();
+		this.className = in.readUTF();
 	}
 }
---
+++ Web/Source/assignment/src/finantix/assignment/part3/main.java
@@ -0,0 +1,7 @@
+package finantix.assignment.part3;
+
+public class main {
+	public static void main(String[] args) {
+		System.out.println("Bonjour");
+	}
+}
