397046cf9d8c6b26fb88a36537bd1e561186130e
Mai Anh Phuong <phuong.maianh@harveynash.vn>
2017-01-03 23:53:23 +0700

    no message

--- Web/Source/assignment/src/finantix/assignment/part4/Test.java
+++ Web/Source/assignment/src/finantix/assignment/part4/Test.java
@@ -1,23 +1,28 @@
 package finantix.assignment.part4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Test {
 
-	public static String shiftByK(String value, Integer k) {
-		if (value == null || value.length() < k) {
-			return value;
+	// 1.
+	public static String shiftTo(String text, Integer position) {
+		if (text == null || text.length() < position) {
+			return text;
 		}
-		char[] s = value.toCharArray();
+		char[] s = text.toCharArray();
 		char[] shift = new char[s.length];
-
 		for (int i = 0; i < s.length; i++) {
-			int index = (i + k) % s.length;
+			int index = (i + position) % s.length;
 			shift[index] = s[i];
 		}
 		return new String(shift);
 	}
 
+	// 2.
 	public static String sortByIndex(String value, Integer start, Integer end) {
 		if (value == null || value.length() < start || value.length() < end) {
 			return value;
@@ -44,6 +49,44 @@ public class Test {
 		return new String(sorted);
 	}
 
+	// 3.
+	public static void searchSubString(char[] text, char[] ptrn) {
+		int ptrnLen = ptrn.length;
+		int txtLen = text.length;
+		int[] b = preProcessPattern(ptrn);
+		int i = 0, j = 0;
+		while (i < txtLen) {
+			while (j >= 0 && text[i] != ptrn[j]) {
+				j = b[j];
+			}
+			i++;
+			j++;
+			// a match is found
+			if (j == ptrnLen) {
+				System.out.println("found substring at index:" + (i - ptrnLen));
+				j = b[j];
+			}
+		}
+	}
+
+	public static int[] preProcessPattern(char[] ptrn) {
+		int i = 0, j = -1;
+		int ptrnLen = ptrn.length;
+		int[] b = new int[ptrnLen + 1];
+
+		b[i] = j;
+		while (i < ptrnLen) {
+			while (j >= 0 && ptrn[i] != ptrn[j]) {
+				j = b[j];
+			}
+			i++;
+			j++;
+			b[i] = j;
+		}
+		return b;
+	}
+
+	// 4.
 	public static String[] bubble_sort(String values[]) {
 		for (int i = 0; i < values.length - 1; i++) {
 			for (int j = i + 1; j < values.length; j++) {
@@ -57,103 +100,14 @@ public class Test {
 		return values;
 	}
 
-	public static int[] preProcessPattern(char[] ptrn) {
-		int i = 0, j = -1;
-		int ptrnLen = ptrn.length;
-		int[] b = new int[ptrnLen + 1];
-
-		b[i] = j;
-		while (i < ptrnLen) {
-			while (j >= 0 && ptrn[i] != ptrn[j]) {
-				// if there is mismatch consider the next widest border
-				// The borders to be examined are obtained in decreasing order
-				// from
-				// the values b[i], b[b[i]] etc.
-				j = b[j];
-			}
-			i++;
-			j++;
-			b[i] = j;
-		}
-		return b;
-	}
-
-	public static void searchSubString(char[] text, char[] ptrn) {
-		int i = 0, j = 0;
-		// pattern and text lengths
-		int ptrnLen = ptrn.length;
-		int txtLen = text.length;
-
-		// initialize new array and preprocess the pattern
-		int[] b = preProcessPattern(ptrn);
-
-		while (i < txtLen) {
-			while (j >= 0 && text[i] != ptrn[j]) {
-				j = b[j];
-			}
-			i++;
-			j++;
-
-			// a match is found
-			if (j == ptrnLen) {
-				System.out.println("found substring at index:" + (i - ptrnLen));
-				j = b[j];
-			}
-		}
-	}
-
-	public static int search(String value, String search) {
-		int count = 0;
-		for (int i = 0; i < value.length(); i++) {
-			boolean found = true;
-			for (int j = 0; j < search.length(); j++) {
-				if (!search.substring(j - i, j - i + 1).equals(value.substring(j, j + 1))) {
-					found = false;
-					break;
-				}
-			}
-			if (found)
-				count++;
-		}
-
-		return count;
-	}
-
+	// 5.
 	public static String longestPalindrome(String s) {
-	    if(s==null || s.length()<=1)
-	        return s;
-	 
-	    int len = s.length();
-	    int maxLen = 1;
-	    boolean [][] dp = new boolean[len][len];
-	 
-	    String longest = null;
-	    for(int l=0; l<s.length(); l++){
-	        for(int i=0; i<len-l; i++){
-	            int j = i+l;
-	            if(s.charAt(i)==s.charAt(j) && (j-i<=2||dp[i+1][j-1])){
-	                dp[i][j]=true;
-	 
-	                if(j-i+1>maxLen){
-	                   maxLen = j-i+1; 
-	                   longest = s.substring(i, j+1);
-	                }
-	            }
-	        }
-	    }
-	 
-	    return longest;
-	}
-	
-	public static String longestPalindrome2(String s) {
 		if (s.isEmpty()) {
 			return null;
 		}
-	 
 		if (s.length() == 1) {
 			return s;
 		}
-	 
 		String longest = s.substring(0, 1);
 		for (int i = 0; i < s.length(); i++) {
 			// get longest palindrome with center of i
@@ -161,19 +115,16 @@ public class Test {
 			if (tmp.length() > longest.length()) {
 				longest = tmp;
 			}
-	 
 			// get longest palindrome with center of i, i+1
 			tmp = helper(s, i, i + 1);
 			if (tmp.length() > longest.length()) {
 				longest = tmp;
 			}
 		}
-	 
+
 		return longest;
 	}
-	 
-	// Given a center, either one letter or two letter, 
-	// Find longest palindrome
+
 	public static String helper(String s, int begin, int end) {
 		while (begin >= 0 && end <= s.length() - 1 && s.charAt(begin) == s.charAt(end)) {
 			begin--;
@@ -182,19 +133,62 @@ public class Test {
 		return s.substring(begin + 1, end);
 	}
 
-	public static void main(String[] args) {
-		Object[][] testcases = { { "hlleo", 1, 3 }, { "ooneefspd", 0, 8 }, { "effort", 2, 4 } };
-		for (int i = 0; i < testcases.length; i++) {
-			System.out.println("Shift:" + shiftByK((String) testcases[i][0], (Integer) testcases[i][1]));
-			System.out.println("Sort:"
-					+ sortByIndex((String) testcases[i][0], (Integer) testcases[i][1], (Integer) testcases[i][2]));
-		}
-		System.out.println(Arrays.toString(
-				bubble_sort(new String[] { "B", "e", "d", "a", "c", "f", "s", "k", "g", "p", "h", "a", "s" })));
+	// 6.
+	public static String longestPalindrome2(String s) {
+		if (s == null || s.length() <= 1)
+			return s;
 
-//		System.out.println(search("sadasda", "da"));
-		searchSubString("sadasadas".toCharArray(), "ada".toCharArray());
-		
-		System.out.println(longestPalindrome2("dabcba"));
+		int len = s.length();
+		int maxLen = 1;
+		boolean[][] dp = new boolean[len][len];
+
+		String longest = null;
+		for (int l = 0; l < s.length(); l++) {
+			for (int i = 0; i < len - l; i++) {
+				int j = i + l;
+				if (s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1])) {
+					dp[i][j] = true;
+
+					if (j - i + 1 > maxLen) {
+						maxLen = j - i + 1;
+						longest = s.substring(i, j + 1);
+					}
+				}
+			}
+		}
+		return longest;
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		// BufferedReader br = new BufferedReader(new
+		// InputStreamReader(System.in));
+		// String line = br.readLine();
+		// System.out.println(line);
+		// int N = Integer.parseInt(line);
+
+		Scanner s = new Scanner(System.in);
+		int N = s.nextInt();
+		int N2 = s.nextInt();
+		String search = s.next();
+		String text = s.next();
+
+		// Object[][] testcases = { { "hlleo", 1, 3 }, { "ooneefspd", 0, 8 }, {
+		// "effort", 2, 4 } };
+		// for (int i = 0; i < testcases.length; i++) {
+		// System.out.println("Shift:" + shiftByK((String) testcases[i][0],
+		// (Integer) testcases[i][1]));
+		// System.out.println("Sort:"
+		// + sortByIndex((String) testcases[i][0], (Integer) testcases[i][1],
+		// (Integer) testcases[i][2]));
+		// }
+		// System.out.println(Arrays.toString(
+		// bubble_sort(new String[] { "B", "e", "d", "a", "c", "f", "s", "k",
+		// "g", "p", "h", "a", "s" })));
+		//
+		//// System.out.println(search("sadasda", "da"));
+		// searchSubString("sadasadas".toCharArray(), "ada".toCharArray());
+		//
+		// System.out.println(longestPalindrome2("dabcba"));
 	}
 }
