09a483228013ea76ec8b7e01a77c81b6ca75dfce
PhuongMaiAnh <anhphuong190190@gmail.com>
2017-08-01 09:06:07 +0700

    Update GreenAcademy buoi 43

--- GreenAcademy/website/src/main/java/com/website/demo/annotation/main/Test.java
+++ GreenAcademy/website/src/main/java/com/website/demo/annotation/main/Test.java
@@ -12,91 +12,83 @@ import com.website.demo.annotation.dao.Student;
 
 public class Test {
 
+	public static Address buildAddres(String street, String district, String city) {
+		Address address = new Address();
+		address.setStreet(street);
+		address.setDistrict(district);
+		address.setCity(city);
+		return address;
+	}
+
+	public static Course buildCourse(String name) {
+		Course course = new Course();
+		course.setCourseName(name);
+		return course;
+	}
+
+	public static HomeWork buildHomeWork(String name, Course course) {
+		HomeWork homeWork = new HomeWork();
+		homeWork.setName(name);
+		homeWork.setCourse(course);
+		return homeWork;
+	}
+
+	public static Student buildStudent(String fname, String lname, Integer age, Address address, Course... courses) {
+		Student student = new Student();
+		student.setFirstName(fname);
+		student.setLastName(lname);
+		student.setAge(age);
+		student.setAddress(address);
+		student.setCourses(new ArrayList<Course>());
+		for (Course course : courses) {
+			student.getCourses().add(course);
+		}
+		return student;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Maven + Hibernate + MySQL");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		session.beginTransaction();
 
-		Address address = new Address();
-		address.setStreet("Nguyen Thi Minh Khai");
-		address.setDistrict("1");
-		address.setCity("HCM");
+		Address address = buildAddres("Nguyen Thi Minh Khai", "1", "HCM");
+		Address address2 = buildAddres("Dinh Tien Hoang", "1", "HCM");
 
-		Address address2 = new Address();
-		address2.setStreet("Dinh Tien Hoang");
-		address2.setDistrict("1");
-		address2.setCity("HCM");
-
-		Course course = new Course();
-		course.setCourseName("Java");
-
-		Course course2 = new Course();
-		course2.setCourseName("Spring");
-
-		Course course3 = new Course();
-		course3.setCourseName("Hibernate");
-
-		Course course4 = new Course();
-		course4.setCourseName("JQuery");
-		
+		Course course = buildCourse("Java");
 		session.save(course);
+		Course course2 = buildCourse("Spring");
 		session.save(course2);
+		Course course3 = buildCourse("Hibernate");
 		session.save(course3);
+		Course course4 = buildCourse("JQuery");
 		session.save(course4);
 
-		HomeWork homeWork = new HomeWork();
-		homeWork.setName("Basic-Java");
-		homeWork.setCourse(course);
+		HomeWork homeWork = buildHomeWork("Basic-Java", course);
 		course.getHomeworks().add(homeWork);
-		HomeWork homeWork2 = new HomeWork();
-		homeWork2.setName("Advance-Java");
-		homeWork2.setCourse(course);
-		course.getHomeworks().add(homeWork2);
-
-		HomeWork homeWork3 = new HomeWork();
-		homeWork3.setName("SpringMVC");
-		homeWork3.setCourse(course2);
-		course2.getHomeworks().add(homeWork3);
-		HomeWork homeWork4 = new HomeWork();
-		homeWork4.setName("SpringSecurity");
-		homeWork4.setCourse(course2);
-		course2.getHomeworks().add(homeWork4);
-
-		HomeWork homeWork5 = new HomeWork();
-		homeWork5.setName("ORM");
-		homeWork5.setCourse(course3);
-		course3.getHomeworks().add(homeWork5);
-
 		session.save(homeWork);
+
+		HomeWork homeWork2 = buildHomeWork("Advance-Java", course);
+		course.getHomeworks().add(homeWork2);
 		session.save(homeWork2);
+
+		HomeWork homeWork3 = buildHomeWork("SpringMVC", course2);
+		course2.getHomeworks().add(homeWork3);
 		session.save(homeWork3);
+
+		HomeWork homeWork4 = buildHomeWork("SpringSecurity", course2);
+		course2.getHomeworks().add(homeWork4);
 		session.save(homeWork4);
+
+		HomeWork homeWork5 = buildHomeWork("ORM", course3);
+		course3.getHomeworks().add(homeWork5);
 		session.save(homeWork5);
 
-		Student student = new Student();
-		student.setFirstName("Phuong");
-		student.setLastName("Mai");
-		student.setAge(29);
-		student.setAddress(address);
-		student.setCourses(new ArrayList<Course>());
-		student.getCourses().add(course);
-		student.getCourses().add(course2);
-		student.getCourses().add(course3);
-		student.getCourses().add(course4);
-
-		Student student2 = new Student();
-		student2.setFirstName("An");
-		student2.setLastName("Tran");
-		student2.setAge(29);
-		student2.setAddress(address);
-		student2.setCourses(new ArrayList<Course>());
-		student2.getCourses().add(course2);
-		student2.getCourses().add(course3);
+		Student student = buildStudent("Phuong", "Mai", 29, address, course, course2, course3, course4);
+		Student student2 = buildStudent("An", "Tran", 29, address2, course2, course3);
 
 		session.save(student);
 		session.save(student2);
 		session.getTransaction().commit();
 		System.out.println("Done");
 	}
-
 }
---
+++ GreenAcademy/website/src/main/java/com/website/demo/xml/main/HQL.java
@@ -0,0 +1,26 @@
+package com.website.demo.xml.main;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import com.website.demo.xml.persistence.HibernateUtil;
+
+public class HQL {
+
+	public static void main(String[] args) {
+		System.out.println("Maven + Hibernate + MySQL");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+
+		Query query = session.createQuery("from student where student_name = :name");
+		query.setParameter("name", "Phuong");
+		List list = query.list();
+		Arrays.toString(list.toArray());
+
+		session.getTransaction().commit();
+		System.out.println("Done");
+	}
+}
